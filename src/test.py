7c9e6bed2e2ad6aa3707132f88fa237cb3daed56
Sashakt-2009 <sashaktchauhan332@gmail.com>
2026-07-10 08:18:14 +0530

    plots + rsme + coefficents and intercepts + doc update

--- src/test.py
+++ src/test.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-import joblib
-from sklearn.metrics import mean_squared_error, r2_score
+import joblib, matplotlib.pyplot as plt
+from sklearn.metrics import root_mean_squared_error, r2_score ,mean_absolute_error
 
 # Load the test dataset.
 df = pd.read_csv("data/raw/sensor_testdata.csv")
@@ -14,8 +14,27 @@ y = np.array(df["True_Temperature"])
 model = joblib.load("models/model1.pkl")
 
 # Make predictions on the test features.
-Y = model.predict(x)
+y_predict = model.predict(x)
 
 # Print evaluation metrics for model performance.
-print(f"mean squared error: {mean_squared_error(y, Y)}")
-print(f"r^2 score: {r2_score(y, Y)}")
+print(f"root mean squared error: {root_mean_squared_error(y, y_predict)}")
+print(f"mean absloute error: {mean_absolute_error(y, y_predict)}")
+print(f"r^2 score: {r2_score(y, y_predict)}")
+residual = y - y_predict
+
+# Print parameter coefficients and intercepts
+print(model.coef_)
+print(model.intercept_)
+
+# Plot a graph between target value and true value
+fig, ax1 = plt.subplots()
+ax1.set_ylabel("True_Temperature")
+ax1.set_xlabel("Predicted_Temperature")
+ax1.scatter(y, y_predict)
+
+fig, ax2 = plt.subplots()
+ax2.scatter(y_predict, residual)
+ax2.set_ylabel("Residuals")
+ax2.set_xlabel("Predicted_Temperature")
+ax2.axhline(y=0, color="red", linestyle="--")
+plt.show()
